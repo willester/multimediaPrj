e48f7354b629fa2f00759f176e825d6676297aa8
willester <willisarivan1@gmail.com>
2020-11-06 19:43:13 +0200

    test functionabilitate about card

--- 1.js
+++ 1.js
@@ -3,8 +3,11 @@ window.onload = () => {
 console.log('test')
 
 const about = document.getElementById('about')
+const aboutCard = document.getElementById('aboutCard')
 about.onclick = () => {
     console.log('test')
+    aboutCard.style.display ='flex'
+    aboutCard.style.flexWrap = 'wrap'
 }
 
 
