eb33efad39069499c26d907bb8e98c8d9a3abc46
willester <willisarivan1@gmail.com>
2020-11-07 15:06:01 +0200

    aboutcard + mai  trb putin lucrat la el

--- 1.js
+++ 1.js
@@ -6,8 +6,8 @@ const about = document.getElementById('about')
 const aboutCard = document.getElementById('aboutCard')
 about.onclick = () => {
     console.log('test')
-    aboutCard.style.display ='flex'
-    aboutCard.style.flexWrap = 'wrap'
+    aboutCard.style.display = aboutCard.style.display === 'block' ? 'none' : 'block'
+    
 }
 
 
