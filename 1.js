439fc65a2316990c7f7f88a4ee2c52d382192c55
willester <willisarivan1@gmail.com>
2020-11-09 12:35:16 +0200

    facut canvasu sa arate ca un ecran

--- 1.js
+++ 1.js
@@ -4,7 +4,11 @@ console.log('test')
 
 const aboutbtn = document.getElementById('about')
 const aboutCard = document.getElementById('aboutCard')
+
 const canvas = document.getElementById('ecranJoc')
+const c = canvas.getContext("2d");
+c.fillRect(0, 0, canvas.width, canvas.height);
+
 
 aboutbtn.onclick = () => {
     
@@ -16,7 +20,7 @@ aboutbtn.onclick = () => {
 
 class Broscoiu
 {
-    
+
 }
 
 
