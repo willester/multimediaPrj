79e0c69537fc8b7d49d443b5d8ea7a8f6e2a04bb
willester <willisarivan1@gmail.com>
2020-11-26 21:00:52 +0200

    title+short description of functionabilities

--- 1.js
+++ 1.js
@@ -5,6 +5,14 @@ console.log('test')
 const aboutbtn = document.getElementById('about')
 const aboutCard = document.getElementById('aboutCard')
 
+let userName = 'anonymous'
+let logged = false;
+
+
+const welcomeU = document.getElementById('welcomeU')
+
+welcomeU.innerHTML = welcomeU.innerHTML.replace('...',userName);
+
 
 aboutbtn.onclick = () => {
     
