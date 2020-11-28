bc3532a5c7275a1a66e2577f0a66b417e72b8696
willester <willisarivan1@gmail.com>
2020-11-28 19:02:30 +0200

    scalat bn posteru 2

--- 1.js
+++ 1.js
@@ -1,6 +1,6 @@
 window.onload = () => {
 
-console.log('test')
+
 
 const aboutbtn = document.getElementById('about')
 const aboutCard = document.getElementById('aboutCard')
@@ -11,18 +11,59 @@ const ceva = document.querySelector('.gridmovieposters')
 let c = document.getElementById("myCanvas");
 let ctx = c.getContext("2d");
 
-let img = new Image();
 
+
+
+let c2 = document.getElementById("myCanvas2");
+let ctx2 = c2.getContext("2d");
+let python = document.getElementById('python')
+
+
+
+
+let img = new Image();
 
 
 img.onload = function(){
     
    c.width = this.width
     c.height = this.height
+    
+    let diferentaX = c.width - python.width
+    let diferentaY = c.height - python.height
 
+
+    c2.width = python.width+diferentaX
+
+    c2.height = python.height+diferentaY
+
+    python.width = c2.width
+    python.height = c2.height
+
+
+   ctx2.drawImage(python,0,0,python.width,python.height)
+    
     ctx.drawImage(img, 0, 0, this.width,this.height);
 }
 img.src = "posters/fantasticPoster.jpg"
+
+
+//470.483x664.6
+
+// python.width = c.width
+// python.height = c.height
+
+
+
+
+
+
+
+
+
+
+
+
 
 c.onclick = () => 
 {
@@ -41,15 +82,10 @@ welcomeU.innerHTML = welcomeU.innerHTML.replace('...',userName);
 
 aboutbtn.onclick = () => {
     
-    console.log('test')
+   
     aboutCard.style.display = aboutCard.style.display === 'block' ? 'none' : 'block'
     
 }
-
-
-
-
-
 
 
 
